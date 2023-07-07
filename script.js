2453dd9bd5dc9a6542dd9d717b2b0596d990ef19
shashwat singh <shashwatsingh@shashwats-MacBook-Air.local>
2023-07-07 08:25:08 +0530

    added js and change ui for mobile

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
 console.log("lets play the game");
+let audioturn = new Audio("ting.mp3")
+let gameover = false;
 let turn = "X";
 
 
 // logic for change turn
+// if the turn is X then return 0 if not return X;
 const changeTurn = ()=>{
-    return turn ==="X"?"O":"X";
+    return turn ==="X"?"0":"X";
 }
+
+// logic to check a win 
+
+const checkWin = ()=>{
+    let celltext = document.getElementsByClassName('celltext');
+    let wins =[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+    wins.forEach(e=>{
+        if((celltext[e[0]].innerText=== celltext[e[1]].innerText)&&(celltext[e[2]].innerText=== celltext[e[1]].innerText)&&(celltext[e[0]].innerText!=="")){
+           document.querySelector('.msg').innerText = celltext[e[0]].innerText +" won!";
+           gameover = true;
+        }
+
+    } )
+}
+
+// game logic
+// array.from will return a html
+// to use for each we should make that as an array
+let cells = document.getElementsByClassName("cell");
+Array.from(cells).forEach(element=>{
+    let celltext = element.querySelector(".celltext");
+    element.addEventListener('click',() =>{
+        if(celltext.innerText===''){
+            celltext.innerText=turn;
+            turn = changeTurn(); 
+            audioturn.play();
+            checkWin();
+            if(!gameover){
+            document.getElementsByClassName("msg")[0].innerText= " turn for " +turn; 
+            }
+        }
+    })
+})
